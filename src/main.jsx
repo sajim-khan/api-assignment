5ced737a0e220832fbc788fff7d5ff375e4989cf
sajim-khan <shossain1427@gmail.com>
2023-08-12 22:25:50 +0600

    use loder for fetch data and set router

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import ViewDetails from "./Details/ViewDetails";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
-    // children: [
-    //   {
-
-    //   }
-    // ]
+    children: [
+      {
+        path: "details/:id",
+        element: <ViewDetails></ViewDetails>,
+        loader: async ({ params }) => {
+          return fetch(
+            `https://api.tvmaze.com/lookup/shows?thetvdb=${params.id}`
+          );
+        },
+      },
+    ],
   },
-
-])
-ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
