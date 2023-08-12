0acdfff47ab3df1e5b7c35ece25676be28d1107f
sajim-khan <shossain1427@gmail.com>
2023-08-12 22:22:20 +0600

    added card design for showing data and fetch url

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Details from "./Details/Details";
+//import Details from "./Details/Details";
 
 const App = () => {
+  const [movieData, setmovieData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.tvmaze.com/search/shows?q=all")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setmovieData(data);
+      })
+      .catch((error) => console.log(`404 page not found ${error}`));
+  }, []);
+  //console.log({ movieData });
   return (
     <>
-      <div className='text-center font-bold text-blue-400 text-4xl mt-5'>This Is Our Home Components 😃</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {movieData?.map((singleData, index) => (
+          <Details key={index} singleData={singleData}></Details>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
+export default App;
 
-export default App
+// <Details key={singleData.id} data={singleData}> </Details>
