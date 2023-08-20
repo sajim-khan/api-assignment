8c86ffe1171fc918ad800de4a1dbabf39e9df114
sajim-khan <shossain1427@gmail.com>
2023-08-20 19:10:16 +0600

    added ternary operator

--- src/state/State.jsx
+++ src/state/State.jsx
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 
 const State = () => {
 
-const [name, setName] = useState('Sajim')
+const [name, setName] = useState('g')
 
 setTimeout(() => {
   setName();
-}, 1000)
+}, 3000)
     return (
-        <div className='m-10 p-5 bg-blue-600 text-white text-3xl' >
-            <h1>Hello { name ? name : 'Guest'}</h1>
-        </div>
+      <div className="m-10 p-5 bg-blue-600 text-white text-3xl">
+        {/* <h1>Hello { name ? name : 'Guest'}</h1> */}
+        {/* {name && <h1>Hello {name} </h1>}
+        {!name && <h1>Hello Guest </h1>} */}
+        {/* //ternary operator */}
+        {name ? <h1>Hello {name} </h1> : <h1>Hello Guest </h1>}
+      </div>
     );
 };
 
